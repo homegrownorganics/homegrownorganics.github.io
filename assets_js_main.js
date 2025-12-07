6bdc046e1ffdb52b60f7b7da8b37baaeb09ad66d
homegrownorganics <homegrownorganicsnz@gmail.com>
2025-12-07 21:51:18 +1300

    Add files via upload

---
+++ assets_js_main.js
@@ -0,0 +1,5 @@
+// main.js
+// Placeholder for future search, filters, map integration, etc.
+console.log("Food Integrity Directory main.js loaded.");
+
+// TODO: Implement search functionality and dynamic filtering
